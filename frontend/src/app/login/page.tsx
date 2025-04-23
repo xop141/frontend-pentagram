0b7775c5d99dce710193e90033797f4cff2e0d9f
xop141 <lowsens400@gmail.com>
2025-04-23 08:04:33 +0800

    Form frontend (#29)
    * Form frontend
    
    * Update page.tsx
    
    background color
    
    * Update page.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,49 +1,76 @@
 'use client'
 
 import React from 'react'
-
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
-import Logo from './loginComponent/logo.svg'
+import Logo from './loginComponent/asd.svg'
 import FbLogin from './loginComponent/fbLogin'
 import SeparatorOr from './loginComponent/SeparatorOr'
 import Jump from './loginComponent/jump'
-
+import loginSchema from './loginComponent/loginSchema'
+import { Formik, useFormik } from 'formik'
+import Image from 'next/image'
 const Page = () => {
+  const formik = useFormik({
+    initialValues: {
+      login: '',
+      password: ''
+    },
+    validationSchema: loginSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  });
   return (
-    <div className='bg-black w-full h-[100vh]'>
-    <div className='h-full flex items-center justify-center flex-col gap-[10px]'>
-      <div className='border border-white/50 w-full max-w-[500px] flex flex-col items-center gap-8 px-6 py-14 rounded-xl'>
-        
-        {/* Logo Section */}
-        <div className='w-full flex justify-center items-center bg-white p-6'>
-          <Logo className="w-32 h-auto text-black" />
-        </div>
-        
-        {/* Input Fields Section */}
-        <div className='flex flex-col gap-5 w-full'>
-          <div className='flex flex-col gap-3'>
-            <Input placeholder='Phone number, username, or email' />
-            <Input placeholder='Password' />
+    <div className="bg-black w-full h-[100vh]">
+      <div className="h-full flex items-center justify-center flex-col gap-[10px]">
+        <div className="border border-white/50 w-full max-w-[500px] flex flex-col items-center gap-8 px-6 py-14 rounded-xl">
+        <div className="w-full p-6 relative">
+    <Image src={Logo} alt="Logo" objectFit="contain" layout="responsive"/>
+  </div>
+          <div className="flex flex-col gap-5 w-full">
+            <form onSubmit={formik.handleSubmit} className="w-full flex flex-col gap-3">
+              <div className="flex flex-col gap-3">
+                <Input
+                  placeholder="Phone number, username, or email"
+                  name="login"
+                  value={formik.values.login}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.login && formik.touched.login && (
+                  <div className="text-red-500 text-xs">{formik.errors.login}</div>
+                )}
+                <Input
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.password && formik.touched.password && (
+                  <div className="text-red-500 text-xs">{formik.errors.password}</div>
+                )}
+              </div>
+              <Button
+                type="submit"
+                variant="ghost"
+                className="bg-blue-500 w-full py-2"
+                disabled={formik.isSubmitting }
+              >
+                Login
+              </Button>
+            </form>
           </div>
-
-          {/* Login Button */}
-          <Button variant="ghost" className='bg-blue-500 w-full py-2'>
-            Login
-          </Button>
+          <SeparatorOr />
+          <FbLogin />
         </div>
-
-       
-        <SeparatorOr />
-        <FbLogin />
+        <Jump pageName="signUp" />
       </div>
-
-     
-      <Jump pageName="signUp"/>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- frontend/src/app/signUp/page.tsx
+++ frontend/src/app/signUp/page.tsx
@@ -1,15 +1,15 @@
+'use client'
 import React from 'react'
-import { createContext } from 'react';
-import { Formik, Form, Field } from 'formik';
-
+import { useFormik } from 'formik'
 import Logo from '@/app/login/loginComponent/logo.svg'
 import FbLogin from '../login/loginComponent/fbLogin'
 import SeparatorOr from '../login/loginComponent/SeparatorOr'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import Jump from '../login/loginComponent/jump'
-import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
+import signSchema from './signComponent/signSchema'
 const Page = () => {
   const formik = useFormik({
     initialValues: {
@@ -18,8 +18,9 @@ const Page = () => {
       username: '',
       fullname: ''
     },
+    validationSchema: signSchema,
     onSubmit: (values) => {
-      console.log('Form values:', values)
+      alert(JSON.stringify(values, null, 2)) 
     }
   })
 
@@ -28,7 +29,7 @@ const Page = () => {
       <div className='bg-black h-full w-full flex items-center justify-center flex-col gap-[10px]'>
         <div className='border border-white/50 w-full max-w-[500px] flex flex-col items-center gap-8 px-[30px] py-14 rounded-xl'>
           <div className='w-full flex justify-center items-center bg-white p-6'>
-            <Logo className='w-32 h-auto text-black' />
+            {/* <Logo className='w-32 h-auto text-black' /> */}
           </div>
 
           <p className='text-center text-white/50'>
@@ -38,13 +39,23 @@ const Page = () => {
           <FbLogin />
           <SeparatorOr />
 
-          <form onSubmit={formik.handleSubmit} className='w-full flex flex-col gap-[10px]'>
-            <Input
-              placeholder='Mobile number or email'
-              name='email'
-              onChange={formik.handleChange}
-              value={formik.values.email}
-            />
+          <form onSubmit={formik.handleSubmit} className='w-full flex flex-col gap-[10px] text-white'>
+       
+
+          <div className='relative'>
+              <Input
+                placeholder='Mobile number or email'
+                name='email'
+                type='email'
+                onChange={formik.handleChange}
+                value={formik.values.email}
+              />
+              {formik.errors.email && formik.touched.email && (
+                <div className='text-red-500'>
+                  {formik.errors.email}
+                </div>
+              )}
+            </div>
             <Input
               placeholder='Password'
               name='password'
@@ -52,24 +63,35 @@ const Page = () => {
               onChange={formik.handleChange}
               value={formik.values.password}
             />
+            {formik.errors.password && formik.touched.password && (
+              <div className='text-red-500'>{formik.errors.password}</div>
+            )}
             <Input
               placeholder='Full name'
               name='fullname'
               onChange={formik.handleChange}
               value={formik.values.fullname}
             />
+            {formik.errors.fullname && formik.touched.fullname && (
+              <div className='text-red-500'>{formik.errors.fullname}</div>
+            )}
+
             <Input
               placeholder='Username'
               name='username'
               onChange={formik.handleChange}
               value={formik.values.username}
             />
-            <Button type='submit' className='bg-blue-500 w-full'>
+            {formik.errors.username && formik.touched.username && (
+              <div className='text-red-500'>{formik.errors.username}</div>
+            )}
+
+            <Button type='submit' className='bg-blue-500 w-full' disabled={formik.isSubmitting}>
               <p>Sign up</p>
             </Button>
           </form>
 
-          <div className='text-center text-[12px] flex flex-col gap-[10px]'>
+          <div className='text-center text-[12px] flex flex-col gap-[10px] text-white/50'>
             <p>
               People who use our service may have uploaded your contact information to Pentagram.{' '}
               <span className='text-blue-100 cursor-pointer hover:underline'>Learn more</span> .
