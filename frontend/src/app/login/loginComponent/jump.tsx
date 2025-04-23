695a9dfcb42cda4694d3a06f4afc39e8bc945599
xop141 <lowsens400@gmail.com>
2025-04-23 08:52:35 +0800

    Update jump.tsx (#30)

--- frontend/src/app/login/loginComponent/jump.tsx
+++ frontend/src/app/login/loginComponent/jump.tsx
@@ -14,7 +14,7 @@ const Jump = ({ pageName }: { pageName: string }) => {
     <div className='w-full max-w-[500px] flex justify-center items-center px-6 py-10 border border-white/50 rounded-xl'>
       <div className='w-fit flex items-center gap-2'>
         <p className='text-white text-center'>
-          {pageName === 'login' ? "Don't have an account?" : "Have an account?"}
+          {pageName === 'login' ? "Have an account?" : "Don't have an account?"}
         </p>
         <p
           className='text-blue-500 hover:text-white cursor-pointer'
